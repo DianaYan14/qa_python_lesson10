e1b790aa3c4cad4cfc3c98a833c9691525888f69
Diana Yanysheva <dintos.pisos@gmail.com>
2023-05-16 00:49:31 +0500

    init

---
+++ tests/test_selene.py
@@ -0,0 +1,12 @@
+from selene import by, be, browser
+from selene.support.shared.jquery_style import s
+
+
+def test_github():
+    browser.open("https://github.com")
+    s(".header-search-input").click()
+    s(".header-search-input").send_keys("eroshenkoam/allure-example")
+    s(".header-search-input").submit()
+    s(by.link_text("eroshenkoam/allure-example")).click()
+    s("#issues-tab").click()
+    s(by.partial_text("С Новым Годом")).should(be.visible)
